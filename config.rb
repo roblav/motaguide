86c3baab5954eb4c29c725e489dcfb5f145d2b94
RobLav <rob@systemseed.com>
2014-11-20 13:10:55 +0000

    Published gh-pages to GitHub pages.

--- config.rb
+++ config.rb
@@ -1,4 +1,7 @@
 # Require any additional compass plugins here.
+# require 'foundation'
+
+# add_import_path "bower_components/foundation/scss"
 
 # Set this to the root of your project when deployed:
 http_path = "/"
